5f62f7b0f07917a606f213cd6ebf55f68961d023
rishabharya <rishabharya2799@gmail.com>
2020-11-09 22:11:25 +0530

    Japanese Cities' Attributes

---
+++ 1.Basic Select/05.Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+/*
+Link - https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+Score - 10.00
+*/
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
