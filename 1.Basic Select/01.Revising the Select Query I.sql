4ff760fb03852912375a6e6e9ff37d46cf96c461
rishabharya <rishabharya2799@gmail.com>
2020-11-09 22:02:56 +0530

    Revising the Select Query I

---
+++ 1.Basic Select/01.Revising the Select Query I.sql
@@ -0,0 +1,5 @@
+/*
+Link - https://www.hackerrank.com/challenges/revising-the-select-query/submissions/code/187135828
+Score - 10.00
+*/
+SELECT * from CITY WHERE CountryCode = 'USA' AND population >100000;
