fac97131666c4a02f1be049bdb8e7f6f79bfbfd4
rishabharya <rishabharya2799@gmail.com>
2020-11-09 22:21:01 +0530

    Weather Observation Station 5

---
+++ 1.Basic Select/10.Weather Observation Station 5.sql
@@ -0,0 +1,6 @@
+/*
+Link - https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+Score - 10.00
+*/
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY),CITY ASC LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1;
